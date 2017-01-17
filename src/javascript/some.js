012c1f8590f970053bcc54f20aa34cea0ba2eb2e
aihe <aiheheichang@gmail.com>
2017-01-17 22:36:40 +0100

    HE IS FUCKING DANCING BOYZ

--- src/javascript/some.js
+++ src/javascript/some.js
@@ -27,6 +27,30 @@ async function eyesHappy() {
     }
 }
 
+async function dancing() {
+    while (autreEnCours) {
+        document.getElementById('title').innerText = "( ^___________^)";
+        var nb = Math.random();
+        console.log(nb);
+        if (nb > 0.333) {
+            document.getElementById('page').style.marginLeft="5%";
+            document.getElementById('title').innerText = "ᕕ( ^___________^)ᕗ";
+            await sleep(350);
+            document.getElementById('page').style.marginLeft="15%";
+        } else if (nb > 0.333 && nb < 0.666) {
+            document.getElementById('page').style.marginLeft="25%";
+            document.getElementById('title').innerText = "(づ ^___________^)づ";
+            await sleep(350);
+            document.getElementById('page').style.marginLeft="15%";
+        } else {
+            document.getElementById('page').style.marginTop="10%";
+            document.getElementById('title').innerText = "ᑫ( ^___________^)ᑷ";
+            await sleep(350);
+            document.getElementById('page').style.marginTop="20%";
+        }
+    }
+}
+
 function play() {
     var audio = document.getElementById('audio');
     if (audio.paused) {
@@ -34,10 +58,13 @@ function play() {
         document.getElementById('dancing').style.display="inline";
         document.getElementById('icone').innerHTML=
         "<i class='fa fa-pause-circle fa-2x' aria-hidden='true'></i>";
+        autreEnCours = true;
+        dancing();
     } else {
         audio.pause();
         audio.currentTime = 0;
         autreEnCours = false;
+        document.getElementById('title').innerText = "( •___________•)";
         document.getElementById('dancing').style.display="none";
         document.getElementById('icone').innerHTML=
         "<i class='fa fa-play-circle fa-2x' aria-hidden='true'></i>";
