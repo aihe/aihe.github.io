7e42387f6d5df13d4933c1acc44d4f289ca2327b
aihe <aiheheichang@gmail.com>
2017-01-17 23:58:46 +0100

    HE IS NOW DANCING EVERYWHERE

--- src/javascript/some.js
+++ src/javascript/some.js
@@ -1,57 +1,79 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+
+
+function sleep(){
+	var waitUntil = new Date().getTime() + ms;
+	while(new Date().getTime() < waitUntil) true;
 }
 
 var autreEnCours = false;
 
-async function eyesClosed() {
+function eyesClosed() {
     if (autreEnCours == false) {
         autreEnCours = true;
         document.getElementById('title').innerText = "( -___________-)";
-        await sleep(350);
-        document.getElementById('title').innerText = "( •___________•)";
-        autreEnCours = false;
+        setTimeout(function(){
+            document.getElementById('title').innerText = "( •___________•)";
+            autreEnCours = false;
+        }, 350);
     }
 }
 
-async function eyesHappy() {
+function eyesHappy() {
     if (autreEnCours == false) {
         autreEnCours = true;
         document.getElementById('page').style.marginTop="10%";
         document.getElementById('title').innerText = "ヽ( ^___________^)ﾉ";
-        await sleep(350);
-        document.getElementById('page').style.marginTop="20%";
-        await sleep(400);
-        document.getElementById('title').innerText = "( •___________•)";
-        autreEnCours = false;
+
+        setTimeout(function(){
+            document.getElementById('page').style.marginTop="20%";
+        }, 400);
+
+        setTimeout(function(){
+            document.getElementById('title').innerText = "( •___________•)";
+            autreEnCours = false;
+        }, 800);
     }
 }
 
-async function dancing() {
-    while(autreEnCours) {
-        document.getElementById('title').innerText = "( ^___________^)";
-        var nb = Math.random();
-        console.log(nb);
-        if (nb < 0.333) {
-            document.getElementById('page').style.marginLeft="5%";
-            document.getElementById('title').innerText = "ᕕ( ^___________^)ᕗ";
-            await sleep(450);
+function dancing() {
+    document.getElementById('title').innerText = "( ^___________^)";
+    var nb = Math.random();
+    console.log(nb);
+
+    if (nb < 0.333) {
+        document.getElementById('page').style.marginLeft="5%";
+        document.getElementById('title').innerText = "ᕕ( ^___________^)ᕗ";
+
+        setTimeout(function(){
             document.getElementById('page').style.marginLeft="15%";
-            await sleep(400);
-        } else if (nb > 0.333 && nb < 0.666) {
-            document.getElementById('page').style.marginLeft="25%";
-            document.getElementById('title').innerText = "(づ ^___________^)づ";
-            await sleep(400);
+        }, 450);
+        setTimeout(function(){
+        }, 850);
+
+    } else if (nb > 0.333 && nb < 0.666) {
+        document.getElementById('page').style.marginLeft="25%";
+        document.getElementById('title').innerText = "(づ ^___________^)づ";
+
+        setTimeout(function(){
             document.getElementById('page').style.marginLeft="15%";
-            await sleep(450);
-        } else if (nb > 0.666) {
-            document.getElementById('page').style.marginTop="10%";
-            document.getElementById('title').innerText = "ᑫ( ^___________^)ᑷ";
-            await sleep(450);
+        }, 450);
+        setTimeout(function(){
+        }, 850);
+
+    } else if (nb > 0.666) {
+        document.getElementById('page').style.marginTop="10%";
+        document.getElementById('title').innerText = "ᑫ( ^___________^)ᑷ";
+
+        setTimeout(function(){
             document.getElementById('page').style.marginTop="20%";
-            await sleep(400);
-        }
+        }, 450);
+        setTimeout(function(){
+        }, 850);
     }
+
+    setTimeout(function(){if (autreEnCours) {
+        dancing();
+    }}, 850);
 }
 
 function play() {
