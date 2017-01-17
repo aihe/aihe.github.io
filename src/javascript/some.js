d3265d64f3bc7145e56819e35e73910d84ec2091
aihe <aiheheichang@gmail.com>
2017-01-17 02:44:22 +0100

    sync problems solved

--- src/javascript/some.js
+++ src/javascript/some.js
@@ -2,19 +2,29 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+var autreEnCours = false;
+
 async function eyesClosed() {
-    document.getElementById('title').innerText = "-___________-";
-    await sleep(350);
-    document.getElementById('title').innerText = "•___________•";
+    if (autreEnCours == false) {
+        autreEnCours = true;
+        document.getElementById('title').innerText = "-___________-";
+        await sleep(350);
+        document.getElementById('title').innerText = "•___________•";
+        autreEnCours = false;
+    }
 }
 
 async function eyesDead() {
-    document.getElementById('page').style.marginTop="10%";
-    document.getElementById('title').innerText = "^___________^";
-    await sleep(350);
-    document.getElementById('page').style.marginTop="20%";
-    await sleep(400);
-    document.getElementById('title').innerText = "•___________•";
+    if (autreEnCours == false) {
+        autreEnCours = true;
+        document.getElementById('page').style.marginTop="10%";
+        document.getElementById('title').innerText = "^___________^";
+        await sleep(350);
+        document.getElementById('page').style.marginTop="20%";
+        await sleep(400);
+        document.getElementById('title').innerText = "•___________•";
+        autreEnCours = false;
+    }
 }
 
 var clignotement = function(){
