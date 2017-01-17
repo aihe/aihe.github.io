0e7d17de4e80d084dedeaf8fd5575f7c59729e75
Aihe <aiheheichang@gmail.com>
2017-01-17 03:40:21 +0100

    Update some.js

--- src/javascript/some.js
+++ src/javascript/some.js
@@ -27,13 +27,34 @@ async function eyesHappy() {
     }
 }
 
+function play(){
+    var audio = document.getElementById('audio');
+    if (audio.paused) {
+        audio.play();
+        autreEnCours = true;
+        document.getElementById('dancing').style.display="inline";
+        document.getElementById('icone').innerHTML=
+        "<i class='fa fa-pause-circle fa-2x' aria-hidden='true'></i>";
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+        autreEnCours = false;
+        document.getElementById('dancing').style.display="none";
+        document.getElementById('icone').innerHTML=
+        "<i class='fa fa-play-circle fa-2x' aria-hidden='true'></i>";
+    }
+}
+
 var clignotement = function(){
-   if (document.getElementById('commandes').style.visibility=='visible'){
-      document.getElementById('commandes').style.visibility='hidden';
-   }
-   else{
-   document.getElementById('commandes').style.visibility='visible';
-   }
+    if (autreEnCours == false) {
+        autreEnCours = true;
+        if (document.getElementById('commandes').style.visibility=='visible'){
+           document.getElementById('commandes').style.visibility='hidden';
+        } else {
+        document.getElementById('commandes').style.visibility='visible';
+        }
+        autreEnCours = false;
+    }
 };
 
 periode = setInterval(clignotement, 800);
